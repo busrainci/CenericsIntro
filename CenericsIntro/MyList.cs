d17e1f3f7de4223b72c41072ccd92501b357df3b
Abdinci <Abdinci@DESKTOP-MCHR1N4>
2022-12-20 18:31:58 +0300

    Add project files.

---
+++ CenericsIntro/MyList.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CenericsIntro
+{
+    class MyList<T>
+    {
+        //count eleman sayısı demek
+
+        T[] items;
+        //constructor : classı herhangi bir yerde new lersen mylist otomatik olarak çalışır. classla aynı isim olduğu için constructor olur.
+        public MyList() // ctor yazıp tab tab yaptık
+        {
+            items = new T[0]; // array oluştu ve sıfır elemanlı oldu.
+        }
+
+ 
+        public void Add(T item) {
+
+            T[] tempArray = items; //tempArray : geçici dizi demektir.
+            items = new T[items.Length + 1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+
+            items[items.Length - 1] = item;
+
+
+        }
+    }
+}
---
+++ CenericsIntro/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CenericsIntro;
+
+
+MyList<string> Isimler = new MyList<string>();
+Isimler.Add("Engin");
